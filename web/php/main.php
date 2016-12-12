58ab83f964efca5b3ea1541f4b00259d65483b9b
Mike Calvo <mike@citronellasoftware.com>
2016-12-11 20:37:30 -0600

    Added 2016

--- web/php/main.php
+++ web/php/main.php
@@ -1,10 +1,17 @@
 <script type="text/javascript">
 
 function populateTracks() {
-	tracks = new Array( 
-	
-	
+	tracks = new Array(
+
+
 <?
+
+# This function reads your DATABASE_URL configuration automatically set by Heroku
+# the return value is a string that will work with pg_connect
+function pg_connection_string() {
+	return "dbname=abcdefg host=****.amazonaws.com port=5432 user=**** password=**** sslmode=require";
+}
+
 $server='10.6.166.232:3306';
 $username="musicsampler";
 $password="J0yD1v1s10n";
@@ -30,8 +37,8 @@ $artist=mysql_result($result,$i,"artist");
 $album=mysql_result($result,$i,"album");
 $comments=htmlspecialchars(mysql_result($result, $i, "comments"));
 
-if ($i > 0 ) { 
-   echo ","; 
+if ($i > 0 ) {
+   echo ",";
 }
 echo "track('$title', '$artist', '$comments')\n";
 $i++;
@@ -39,15 +46,15 @@ $i++;
 
 mysql_close($link);
 ?>
-); 
-	
+);
+
 	for (var i=0; i < tracks.length; i++) {
 		// Preload the image
 		var preload = new Image(200,200);
 		preload.src='covers/'+(i+1)+'.jpg';
 		preload.align='left';
 		images[i] = preload;
-		
+
 		var row = new Element('div', {'id': i, 'class': 'track' });
 		row.update("Track "+(i+1)+" - \""+tracks[i].title+"\" by "+tracks[i].artist);
 		row.observe('mouseover', handleMouseOver);
